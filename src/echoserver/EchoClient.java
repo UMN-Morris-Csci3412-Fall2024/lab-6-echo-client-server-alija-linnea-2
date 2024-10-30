a0eb8f219d8c8f11d22ebbebf1973c3bb3ce8263
LinneaMay <Linneamay@gilbertson.net>
2024-10-29 21:51:38 -0500

    editing echoclient to pass all tests

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -8,50 +8,46 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.io.*;
 
-public class EchoClient {
-    public static final int portNumber = 6013;
 
-    public static void main(String[] args) {
-        String server;
-        // Use "127.0.0.1", i.e., localhost, if no server is specified.
-        if (args.length == 0) {
-            server = "127.0.0.1";
-        } else {
-            server = args[0];
-        }
-        
+public class EchoClient{
+  public static final int portNumber = 6013;
 
-        try {Socket socket = new Socket(server, portNumber);
-             DataInputStream in = new DataInputStream(socket.getInputStream());
-             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-             InputStreamReader reader = new InputStreamReader(System.in); 
-
-            byte userInput;
-            byte inputByte;
-
-              try{
-                while((userInput = (byte) reader.read()) != -1){
-                  out.writeByte(userInput);
-                  out.flush(); // Ensure all bytes are sent
-                  try {
-                      inputByte = in.readByte(); // Read a byte
-                      System.out.print((char)inputByte); // Print the byte received from the server
-  
-                  } catch (EOFException e) {
-                    break;
-                  }
-                }         
-            }
-              finally {
-                socket.close();
-              }
-        } catch (IOException ioe) {
-            System.out.println("We caught an unexpected exception");
-            System.err.println(ioe);
-        }
+  public static void main(String[] args) {
+    String server;
+    // Use "127.0.0.1", i.e., localhost, if no server is specified.
+    if (args.length == 0) {
+        server = "127.0.0.1";
+    } else {
+        server = args[0];
     }
+
+    try{
+      Socket socket = new Socket(server, portNumber);
+      InputStream userInput = System.in;
+      OutputStream out = socket.getOutputStream();
+      InputStream in = socket.getInputStream();
+
+      int data;
+      while((data = userInput.read()) != -1){
+        out.write(data);
+        out.flush();
+
+        int response = in.read();
+        System.out.write(response);
+        System.out.flush();
+      }
+      socket.close();
+    }
+      catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+  }
+
+  }
 }
+
 
 
 
